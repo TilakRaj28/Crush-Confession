4ff147169ffaf658956bfb4cddae2165dd04ac1a
Tilak Raj <122775556+TilakRaj28@users.noreply.github.com>
2024-02-07 16:41:00 +0530

    1st

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Why You Love ME",
+    introTitle: 'Why I Love You',
+    introDesc: `Kitna pyar hai tumse yeh jan lo
+    Tum hi zindagi ho meri Is baat ko maan lo
+    Tumhe dene ko mere paas Kuchh bhi nahi
+    Bas ek jaan hai Jab ji chahe maang lo.. `,
+    btnIntro: 'Click Here',
+    title: 'Now Your Turn Tell my Why You Love Me  🥰',
+    desc: 'I Know Im So Important For You ',
+    btnYes: 'I Love You',
+    btnNo: 'No I dont Love You',
+    question: 'Tell Me Why You Love ME ',
+    btnReply: 'Send',
+    reply: 'You  are the Best part of my life I feel Blessed You are In my Life 😘😘',
+    mess: 'Thank you Baby I Know You Love Me 🥰😘😘',
+    messDesc: 'I Also Feel Blessed You are in my Life',
+    btnAccept: 'Love You 2',
+}
